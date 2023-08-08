eac67c2af2d0b599da2ad72c77545273939ad414
xiao_wine <xiaowine0@gmail.com>
2023-08-08 23:28:42 +0800

    add Activity add SimpleHttpClient

--- app/src/main/java/cn/xiaowine/ktl/MainActivity.kt
+++ app/src/main/java/cn/xiaowine/ktl/MainActivity.kt
@@ -1,15 +1,10 @@
-package cn.xiaowine.ktl;
+package cn.xiaowine.ktl
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Activity
+import android.os.Bundle
 
-import cn.xiaowine.xkt.AcTool;
-import cn.xiaowine.xkt.Tool;
-import kotlin.jvm.functions.Function1;
-
-public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 }
---
+++ xkt/src/main/java/cn/xiaowine/xkt/SimpleHttpClient.kt
@@ -0,0 +1,31 @@
+package cn.xiaowine.xkt
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+//HttpURLConnection太基础了，除了拿来简单的get，其他的都不太好用，所以这里就不写了，建议复杂的还是使用Okhttp
+object SimpleHttpClient {
+
+    /**
+     * Get
+     *
+     * @param connectTimeout
+     * @param instanceFollowRedirects
+     * @param onSuccess
+     * @param onError
+     */
+    fun String.get(connectTimeout: Int = 5000, instanceFollowRedirects: Boolean = true, onSuccess: ((String) -> Unit)? = null, onError: ((Exception) -> Unit)? = null) {
+        try {
+            val connection = (URL(this).openConnection() as HttpURLConnection).apply {
+                this.connectTimeout = connectTimeout
+                requestMethod = "GET"
+                this.instanceFollowRedirects = instanceFollowRedirects
+            }
+            val reader = BufferedReader(InputStreamReader(connection.inputStream))
+            onSuccess?.invoke(reader.readText())
+        } catch (e: Exception) {
+            onError?.invoke(e)
+        }
+    }
+}
