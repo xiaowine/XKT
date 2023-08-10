4d9141e449aa2ddff5c52d5dd7bbfd7bce58a136
xiao_wine <xiaowine0@gmail.com>
2023-08-10 20:37:21 +0800

    1.0.4

--- xkt/build.gradle.kts
+++ xkt/build.gradle.kts
@@ -38,7 +38,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.xiaowine"
                 artifactId = "XKT"
-                version = "1.0.3"
+                version = "1.0.4"
                 from(components["release"])
             }
         }
