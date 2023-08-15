ac9a6dbd04a62824f9beddb9fd06fa6a1e35115e
xiao_wine <xiaowine0@gmail.com>
2023-08-15 21:33:14 +0800

    add getRoundedCornerBitmap

---
+++ xkt/src/main/java/cn/xiaowine/xkt/ViewTool.kt
@@ -0,0 +1,24 @@
+package cn.xiaowine.xkt
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
+
+class ViewTool {
+    private fun Bitmap.getRoundedCornerBitmap(cornerRadius: Float): Bitmap {
+        val output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(output)
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        val rectF = RectF(0f, 0f, width.toFloat(), height.toFloat())
+        paint.color = Color.BLACK
+        canvas.drawRoundRect(rectF, cornerRadius, cornerRadius, paint)
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(this, 0f, 0f, paint)
+        return output
+    }
+
+}
