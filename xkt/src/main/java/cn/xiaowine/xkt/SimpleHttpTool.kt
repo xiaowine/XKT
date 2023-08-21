13c86225538ff36977dff77af8c10f7e9686ff50
xiao_wine <xiaowine0@gmail.com>
2023-08-21 18:45:04 +0800

    add isHttpUrl isHttpUrl isUrl

--- xkt/src/main/java/cn/xiaowine/xkt/SimpleHttpTool.kt
+++ xkt/src/main/java/cn/xiaowine/xkt/SimpleHttpTool.kt
@@ -4,6 +4,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.regex.Pattern
 
 //HttpURLConnection太基础了，除了拿来简单的get，其他的都不太好用，所以这里就不写其他请求了，建议复杂的还是使用Okhttp
 object SimpleHttpTool {
@@ -28,5 +29,29 @@ object SimpleHttpTool {
         } catch (e: Exception) {
             onError?.invoke(e)
         }
+    }
+
+    fun isUrl(urls: String): Boolean {
+        val regex = ("(((https|http)?://)?([a-z0-9]+[.])|(www.))"
+                + "\\w+[.|\\/]([a-z0-9]{0,})?[[.]([a-z0-9]{0,})]+((/[\\S&&[^,;\u4E00-\u9FA5]]+)+)?([.][a-z0-9]{0,}+|/?)")
+        val pat = Pattern.compile(regex.trim { it <= ' ' })
+        val mat = pat.matcher(urls.trim { it <= ' ' })
+        return mat.matches()
+    }
+
+    fun isHttpUrl(urls: String): Boolean {
+        val regex = ("(((http)?://)?([a-z0-9]+[.])|(www.))"
+                + "\\w+[.|\\/]([a-z0-9]{0,})?[[.]([a-z0-9]{0,})]+((/[\\S&&[^,;\u4E00-\u9FA5]]+)+)?([.][a-z0-9]{0,}+|/?)")
+        val pat = Pattern.compile(regex.trim { it <= ' ' })
+        val mat = pat.matcher(urls.trim { it <= ' ' })
+        return mat.matches()
+    }
+
+    fun isHttpsUrl(urls: String): Boolean {
+        val regex = ("(((https)?://)?([a-z0-9]+[.])|(www.))"
+                + "\\w+[.|\\/]([a-z0-9]{0,})?[[.]([a-z0-9]{0,})]+((/[\\S&&[^,;\u4E00-\u9FA5]]+)+)?([.][a-z0-9]{0,}+|/?)")
+        val pat = Pattern.compile(regex.trim { it <= ' ' })
+        val mat = pat.matcher(urls.trim { it <= ' ' })
+        return mat.matches()
     }
 }
